94f7fd5d62a8ffdb4a2870f0cc32a116bdfaae23
Eric Kidder <ekidder@salesforce.com>
2019-05-19 13:38:18 -0400

    Better commit

--- carpi.py
+++ carpi.py
@@ -1,14 +1,77 @@
+import asyncio
+import datetime
+
 import obd
 
-connection = obd.OBD("/dev/rfcomm0") # auto-connects to USB or RF port
-if (connection.is_connected()):
-  cmd = obd.commands.SPEED # select an OBD command (sensor)
-  response = connection.query(cmd) # send the command, and parse the response
-  print(response.value) # returns unit-bearing values thanks to Pint
-  print(response.value.to("mph")) # user-friendly unit conversions
+OBD_COMMANDS = [
+    obd.commands.RPM,
+    obd.commands.SPEED,
+    obd.commands.FUEL_STATUS,
+    obd.commands.STATUS,
+    obd.commands.INTAKE_TEMP,
+    obd.commands.AIR_STATUS,
+    obd.commands.FUEL_LEVEL,
+    obd.commands.RELATIVE_ACCEL_POS,
+    obd.commands.FUEL_RATE,
+    obd.commands.FUEL_TYPE,
+    obd.commands.ETHANOL_PERCENT,
+    obd.commands.GET_DTC
+]
 
-  cmd = obd.commands.GET_DTC
-  response = connection.query(cmd)
-  print(response.value)
-else:
-  print("Not connected")
+
+class CarPi:
+    def __init__(self, port):
+        self.data = []
+        self.port = port
+        self.obd = None
+        self.loop = None
+
+    def start(self):
+        self.obd = obd.Async(self.port)
+
+        for command in OBD_COMMANDS:
+            self.obd.watch(command)
+
+        self.loop = asyncio.get_event_loop()
+
+        self.obd.start()
+        self.loop.call_soon(self.handle_interval, self)
+        self.loop.run_forever()
+
+    def stop(self):
+        self.loop.stop()
+        self.obd.stop()
+
+    def handle_interval(self):
+        if self.obd.is_connected() == False:
+            self.loop.call_later(5, self.handle_interval, self)
+            return
+        print("Querying OBD connection")
+
+        current_data = {
+            "timestamp": datetime.datetime.now()
+        }
+
+        for command in OBD_COMMANDS:
+            current_data[command] = self.obd.query(command)
+
+        self.loop.call_later(60, self.handle_interval, self)
+
+    # RPM
+    # SPEED
+    # FUEL_STATUS
+    # STATUS
+    # INTAKE_TEMP
+    # AIR_STATUS
+    # FUEL_LEVEL
+    # RELATIVE_ACCEL_POS
+    # OIL_TEMP
+    # FUEL_RATE
+    # FUEL_TYPE
+    # ETHANOL_PERCENT
+    # GET_DTC
+
+
+if __name__ == "__main__":
+    car_pi = CarPi("/dev/rfcomm0")
+    car_pi.start()
